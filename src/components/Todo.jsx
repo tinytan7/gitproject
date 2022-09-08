186f8ef5415a9ed4a15d4c5cf44b0bb23c8e5334
aishwaray <aish.stha07@gmail.com>
2022-09-08 11:29:43 +0545

    new todos

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,137 +2,124 @@ import React, { useState } from 'react'
 import './Todo.css'
 
 const Todo = () => {
-    const [input, setInput] = useState('');
-    const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos')) );
-    //const[todos, setTodods]=useState([
-        //'Take a walk.',
-        //'Make a presentation.',
-        //'cook food.',
-        //'feed the dog.'
+	const [input, setInput] = useState('');
+	const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos')) || []);
 
-    //]);
- 
+	console.log(input)
+	console.log(todos)
 
-    console.log(input)
-    console.log(todos)
-    
-    //Local Storage Setup
-    // const getLocalTodos = () => {
-    //     if (localStorage.getItem('todos') == null) {
-    //         localStorage.getItem('todos',JSON.stringify([]));
-    //     } else {
-    //         let todoLocal = JSON.parse(localStorage.getItem('todos'));
-    //         setTodos(todoLocal);
-    //     }
-       
-    // }
-    
-        //set items to localStorage
-        const saveLocalTodos = (todo) => {
-            //check if there is already items in localStorage
-            if(localStorage.getItem('todos') == null) {
-               localStorage.setItem('todos',JSON.stringify([]));
-            }else{
-                let todoLocal=JSON.parse(localStorage.getItem('todos'));
-                todoLocal.push(todo);
-                localStorage.setItem('todos',JSON.stringify(todoLocal));
-            }
-        }
+	// Local Storage Setup
+	// const getLocalTodos = () => {
+	// 	if (localStorage.getItem('todos') === null) {
+	// 		localStorage.setItem('todos', JSON.stringify([]));
+	// 	} else {
+	// 		let todoLocal = JSON.parse(localStorage.getItem('todos'));
+	// 		setTodos(todoLocal);
+	// 	}
+	// }
 
-                //CREATE: Add a new todo
-                function handleSubmit(){
-                    setTodos([...todos, input])
-                    saveLocalTodos(input)
-                    setInput('')
-                }
+	// set items to localStorage
+	const saveLocalTodos = (todo) => {
+		// check if there is already items in localStorage
+		if (localStorage.getItem('todos') === null) {
+			localStorage.setItem('todos', JSON.stringify([]));
+		} else {
+			let todoLocal = JSON.parse(localStorage.getItem('todos'));
+			todoLocal.push(todo);
+			localStorage.setItem('todos', JSON.stringify(todoLocal));
+		}
+	}
 
-    //Update" Update the individual items in the array having index
-    function updateTodo(index){
-        const newTodos = [...todos];
-        //newTodos[index] = input;
-        newTodos.splice(index,1,input);
-        setTodos(newTodos);
-        setInput('');
+		// CREATE: Add a new todo
+		function handleSubmit() {
+			setTodos([...todos, input])
+			saveLocalTodos(input)
+			setInput('')
+		}
 
-    }
-    
-    //Delete: Delete te individual items in te array having index
-    function deleteTodo(index){
-        const newTodos = [...todos];
-        newTodos.splice(index,1);
-        setTodos(newTodos);
-    }
-    //Remove All: It removes all te items in te array
-    function removeall() {
-        setTodos([])
-        localStorage.setItem('todos',JSON.stringify([]))
-    }
+		//UPDATE: Update the individual items in the array having index
+		function updateTodo(index) {
+			const newTodos = [...todos];
+			// newTodos[index] = input;
+			newTodos.splice(index, 1, input);
+			setTodos(newTodos);
+			setInput('');
+		}
 
-  return (
-    <div className='todo'>
-        <div className="todo-logo">
-            <h3>Todo App</h3>
-        </div>
+		//DELETE: Delete the individual items in the array having index
+		function deleteTodo(index) {
+			const newTodos = [...todos];
+			newTodos.splice(index, 1);
+			setTodos(newTodos);
+		}
 
-        <div className="todo-form">
-            <form>
-                <input
-                 type="text"
-                 className="task" 
-                 name="input"
-                 value={input}
-                onchange={e => setInput(e.target.value)} 
-                />
-                <input
-                 type="submit"
-                 className="button" 
-                 name="button"
-                  value="Add Todo"
-                  disabled={!input}
-                  onClick={handleSubmit}
-                  />
+		// Remove All: It removes all the items in the array
+		function removeall() {
+			setTodos([])
+			localStorage.setItem('todos', JSON.stringify([]))
+		}
 
-            </form>
+		return (
+			<div className='todo'>
+				<div className="todo-logo">
+					<h3>Todo App</h3>
+				</div>
 
-        </div>
-        <div className="todo-list">
-            <h3>Task List</h3>
-            <ul>
-                {
-                    todos.map((todo) =>(
-                        <li>
-                            {todo}
-                            <button
-                             classname='update'
-                             onClick={()=> updateTodo(todos.indexOf(todo))}
-                             >
-                                Update
-                             </button>
-                             <button
-                             className='delete'
-                             onClick={() =>deleteTodo(todos.indexOf(todo))}
-                             >
-                                Delete
-                             </button>
-                            </li>
-                    ))
-                }
-                
-            </ul>
+				<div className="todo-form">
+					<form>
+						<input
+							type="text"
+							className="task"
+							name="input"
+							value={input}
+							onChange={e => setInput(e.target.value)}
+						/>
+						<input
+							type="submit"
+							className="button"
+							name="button"
+							value="Add Todo"
+							disabled={!input}
+							onClick={handleSubmit}
+						/>
+					</form>
+				</div>
 
-        </div>
+				<div className="todo-lists">
+					<h3>Task Lists</h3>
+					{/*  */}
+					<ul>
+						{todos.map((todo) => (
+							<li>
+								{todo}
+								<button
+									className='update'
+									onClick={() => updateTodo(todos.indexOf(todo))}
+								>
+									Update
+								</button>
+								<button
+									className='delete'
+									onClick={() => deleteTodo(todos.indexOf(todo))}
+								>
+									Delete
+								</button>
+							</li>
+						))
+						}
+					</ul>
+				</div>
 
-        <div className="todo-removeall">
-            <button
-                className='button'
-                onClick={removeall}
-                >
-                    Remove All
-                </button>
+				<div className="todo-removeall">
+					<button
+						className='button'
+						onClick={removeall}
+					>
+						Remove All
+					</button>
+				</div>
+			</div>
+		)
+	}
 
-        </div>
-    </div>
-  )
-}
-
-export default Todo
+	export default Todo
